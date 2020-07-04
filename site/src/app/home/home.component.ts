e071ff8a51ee6b0a4533e56fb43a228ec8bcab1e
Christopher P. Rieger <christopherrieger@hotmail.com>
2020-07-04 18:52:48 -0500

    v10 working base.

--- site/src/app/home/home.component.ts
+++ site/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import {trigger, style, transition, animate, keyframes, query, stagger} from "@angular/animations";
-import { DataService } from "../data.service";
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
--- siteV10/src/app/app-routing.module.ts
+++ siteV10/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "../../src/app/home/home.component";
+import {AboutComponent} from "../../src/app/about/about.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [  {
+  path: '',
+  component: HomeComponent
+},
+  {
+    path: 'about/:id',
+    component: AboutComponent
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
