b67cca3647f2d55f23ffe6368d955110511741e8
Christopher P. Rieger <christopherrieger@hotmail.com>
2020-07-04 17:04:35 -0500

    Remove animation.

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 //This is broke for some reason. I can't figure out why my local hates this. Possible TS conversion issue. Saving for later.
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+//import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
--- site/src/app/home/home.component.ts
+++ site/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {trigger, style, transition, animate, keyframes, query, stagger} from "@angular/animations";
+// import {trigger, style, transition, animate, keyframes, query, stagger} from "@angular/animations";
 import { DataService } from "../data.service";
 
 @Component({
@@ -7,25 +7,25 @@ import { DataService } from "../data.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   animations: [
-    trigger('goals', [
-      transition('* => *', [
-        query(':enter', style({opacity: 0}), {optional: true}),
-
-        query(':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1})
-          ]))]), {optional: true}),
-
-        query(':leave', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
-          ]))]), {optional: true})
-      ])
-    ])
+    // trigger('goals', [
+    //   transition('* => *', [
+    //     query(':enter', style({opacity: 0}), {optional: true}),
+    //
+    //     query(':enter', stagger('300ms', [
+    //       animate('.6s ease-in', keyframes([
+    //         style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+    //         style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+    //         style({opacity: 1, transform: 'translateY(0)', offset: 1})
+    //       ]))]), {optional: true}),
+    //
+    //     query(':leave', stagger('300ms', [
+    //       animate('.6s ease-in', keyframes([
+    //         style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+    //         style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+    //         style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+    //       ]))]), {optional: true})
+    //   ])
+    // ])
   ]
 })
 export class HomeComponent implements OnInit {
